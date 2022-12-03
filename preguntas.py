3137940d4056ef53d238a4f08ffc7b16a4e619ce
juancho1035 <haljuan@hotmail.com>
2022-12-03 13:18:01 -0500

    entrega

--- preguntas.py
+++ preguntas.py
@@ -223,7 +223,7 @@ def pregunta_03():
     # Cree un pipeline que contenga el columnTransformer y el modelo SVC.
     pipeline = pipeline(
         steps=[
-            ("transformer", columnTransformer),
+            ("transformer", columnTransformer()),
             ("SVC", SVC(kernel="linear")),
         ],
     )
